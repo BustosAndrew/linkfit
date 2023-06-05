dce65037ed8cf22b2dafa7115a5a7cd5fdd6aadb
Andrew Bustos <infernious@live.com>
2023-06-04 17:28:10 -0700

    updated site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://linkfit-six.vercel.app/",
+  site: "https://linkfit.net/",
   integrations: [
     tailwind(),
     image({
